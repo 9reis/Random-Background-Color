b961f1f274c17e0024c86942597c449a13268474
Lucas Reis <lucasreis_9@hotmail.com>
2022-07-31 18:36:41 -0300

    Alterando a cor do background utilizando Math round e Math random

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let changeColor = () => {
+    document.body.style.backgroundColor =
+    'rgb(' + Math.round(Math.random() * 255) +
+    ',' + Math.round(Math.random() * 255 ) +
+    ',' + Math.round(Math.random() * 255 ) + ')';
+}
